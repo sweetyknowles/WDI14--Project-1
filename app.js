2a0163f18bdeb1193333c21d953949f6df789d6b
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-02-06 16:12:15 -0500

    changes to the code in app.jss

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ window.onload = function() {
 
 // var space;              // Number of spaces in word '-'
 
-
-
+var actors = ['brad','jennifer', 'darren'];
 var stopButton = document.querySelector('#stopButton');
 }
 
+function actors(){
+    rand = Math.floor(Math.random ()*actors.length);
+    word = actors[rand];
+    document.getElementById()
+}
 
-
-
+var word = "kangaroo";
 
 
 
@@ -32,13 +35,26 @@ let alphabetFunction = function(letters){
     for (i = 0; i < letters.length; i++){
         newArray.push(letters[i])
         let toButtonForm = letters[i]
-        let pushClassOut = $('main').append(`<button class='style'>${letters[i]}</button>`)
+        let pushClassOut = $('main').append(`<button id=${letters[i]} class='style'>${letters[i]}</button>`)
         console.log(letters[i])
     }
 }
 
 alphabetFunction(alphabet)
-
+function compareValues(val){
+    let newWord = word.split('')
+for(let i = 0; i < newWord.length; i++ ){
+    if(val === newWord[i]){
+        console.log('values match this many times')
+    } else {
+        console.log('try again')
+    }
+}
+}
+$('.style').click((target)=> {
+    let value = target.target.id
+    compareValues(value)
+})
 
 
 
